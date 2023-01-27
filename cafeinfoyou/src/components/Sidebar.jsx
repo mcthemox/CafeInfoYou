475d4c124e64ef731e01aa4e255e9a767a221fab
박현목 <phmok1@gmail.com>
2023-01-27 16:43:25 +0900

    error as

--- cafeinfoyou/src/components/Sidebar.jsx
+++ cafeinfoyou/src/components/Sidebar.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import '../styles/sidebar.css';
 
 export default function Sidebar() {
+    
+
     return (
-        <div>
+        <div className='sidebar-container'>
             <div className='sidebar'>
-                사이드바
+                <button className='sidebar-button'>X</button>
             </div>
         </div>
 )}
