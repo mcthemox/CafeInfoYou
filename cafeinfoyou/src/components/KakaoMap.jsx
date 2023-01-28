0bf79bd1d29c8d9c53f45c8692f83a5d5b13e967
HaeRi Seo <jackie100425@naver.com>
2023-01-28 18:03:03 +0900

    feat: redux 수정

--- cafeinfoyou/src/components/KakaoMap.jsx
+++ cafeinfoyou/src/components/KakaoMap.jsx
@@ -1,24 +1,18 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import '../styles/map.css';
 
-const {kakao} = window
+const { kakao } = window;
 
 export default function KakaoMap() {
-  const [map,setMap] = useState(null)
+  const [map, setMap] = useState(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     const container = document.getElementById('map');
-        const options = { center: new kakao.maps.LatLng(33.450701, 126.570667) };
-        const kakaoMap = new kakao.maps.Map(container, options);
-        setMap(kakaoMap);
-  },[])
+    const options = { center: new kakao.maps.LatLng(37.5656, 126.9769) };
+    const kakaoMap = new kakao.maps.Map(container, options);
+    setMap(kakaoMap);
+  }, []);
 
-  
-  
-    return (
-
-  <div id="map"></div>
-
-  )
+  return <div id="map"></div>;
 }
