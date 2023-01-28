d20a71ffc18c8fa91d2acfcb908e0cf195aa1960
박현목 <phmok1@gmail.com>
2023-01-28 16:28:42 +0900

    map,button as

--- cafeinfoyou/src/components/KakaoMap.jsx
+++ cafeinfoyou/src/components/KakaoMap.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
+import '../styles/map.css';
 
 const {kakao} = window
 
@@ -14,7 +15,7 @@ export default function KakaoMap() {
   },[])
     return (
 
-  <div id="map" style={{width:'100%',height: '100vh'}}></div>
+  <div id="map"></div>
 
   )
 }
