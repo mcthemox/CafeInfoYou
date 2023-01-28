7c4c8df5c2be013953a471029fb66681c030dce3
박현목 <phmok1@gmail.com>
2023-01-28 14:49:51 +0900

    sidebar

---
+++ cafeinfoyou/src/components/SidebarContents.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SidebarContents() {
+  return (
+    <div>SidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContentsSidebarContents</div>
+  )
+}
