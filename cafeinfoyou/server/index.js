5c0dd75b113a49eb72655337ad517f4578aa38b8
namhyong <namhyong2@gmail.com>
2023-01-28 16:14:16 +0900

    feat:kakakomap

--- cafeinfoyou/server/index.js
+++ cafeinfoyou/server/index.js
@@ -23,6 +23,21 @@ app.use('/blogSearch', (req, res) => {
   });
 });
 
+app.get('/searchPlace', (req, res) => {
+  const place = '홍대 떡볶이';
+  axios({
+    url: 'https://dapi.kakao.com/v2/local/search/keyword.json?',
+    params: {
+      query: place,
+    },
+    headers: {
+      Authorization: 'KakaoAK 8f5bcb0482043cceab2715b45665b51b',
+    },
+  }).then((res) => {
+    console.log(res.data);
+  });
+});
+
 app.listen(port, () => {
   console.log('Server open : ', port);
 });
