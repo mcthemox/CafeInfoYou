f90bf20a7f53a45b206d3fa1a5b5d9fb7a1a22b2
HaeRi Seo <jackie100425@naver.com>
2023-01-31 15:56:52 +0900

    feat: filter style

--- cafeinfoyou/src/store/modules/filter.js
+++ cafeinfoyou/src/store/modules/filter.js
@@ -1,4 +1,22 @@
-const initState = {};
+const initState = {
+  list: [
+    {
+      id: 0,
+      text: '리뷰많은순',
+      ckeck: false,
+    },
+    {
+      id: 1,
+      text: '대형',
+      ckeck: false,
+    },
+    {
+      id: 2,
+      text: '디저트',
+      ckeck: false,
+    },
+  ],
+};
 
 export default function filter(state = initState, action) {
   return state;
