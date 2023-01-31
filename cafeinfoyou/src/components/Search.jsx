3b8740ee28cd8fa5f8b852b332158f7237dde8cb
HaeRi Seo <jackie100425@naver.com>
2023-01-31 17:52:11 +0900

    feat: filter state 추가

--- cafeinfoyou/src/components/Search.jsx
+++ cafeinfoyou/src/components/Search.jsx
@@ -1,33 +1,37 @@
-import React, { useRef } from 'react'
-import {useDispatch} from 'react-redux'
+import React, { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import '../styles/sidebar.css';
-import axios from 'axios'
+import axios from 'axios';
 
 export default function Search() {
-  const searchContent = useRef()
-  const dispatch = useDispatch()
-  function sendTextValueHanler(){
-    const inputVal = searchContent.current.value
+  const searchContent = useRef();
+  const dispatch = useDispatch();
+  function sendTextValueHandler() {
+    const inputVal = searchContent.current.value;
     axios({
-      method: "get",
+      method: 'get',
       url: 'http://localhost:3001/searchPlace',
       params: {
-        value: inputVal
-      }
-    }).then((searchdata)=>{
-      if(searchdata.status !== 200) return alert('통신에러')
-      const data = searchdata.data
-      console.log("리절:",data);
-      dispatch({type:'INP_VAL', text:data})
-    })
-    
-    
+        value: inputVal,
+      },
+    }).then((searchdata) => {
+      if (searchdata.status !== 200) return alert('통신에러');
+      const data = searchdata.data;
+      console.log('리절:', data);
+      dispatch({ type: 'INP_VAL', text: data });
+    });
   }
+
   return (
     <div>
-      <input ref={searchContent} className='search' />
-      <input  type='image' onClick={sendTextValueHanler} className='search-icon' src='/images/search.png'/>
-      {/* <button onClick={sendTextValueHanler}>검색</button> */}
+      <input ref={searchContent} className="search" />
+      <input
+        type="image"
+        onClick={sendTextValueHandler}
+        className="search-icon"
+        src="/images/search.png"
+      />
+      {/* <button onClick={sendTextValueHandler}>검색</button> */}
     </div>
-  )
+  );
 }
