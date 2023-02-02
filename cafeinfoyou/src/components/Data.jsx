deaaed5888ca0278ea053d9358446f9e459688c7
HaeRi Seo <jackie100425@naver.com>
2023-02-02 15:26:13 +0900

    feat: filter 수정

--- cafeinfoyou/src/components/Data.jsx
+++ cafeinfoyou/src/components/Data.jsx
@@ -1,50 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import cafe from '../store/modules/cafe';
 import '../styles/sidebar.css';
-import axios from 'axios'
+import axios from 'axios';
 
 export default function Data() {
-
-
-  const cafeinfo = useSelector((state) => state.cafe.text)
-  console.log('데이터:', cafeinfo)
-
-
+  const cafeinfo = useSelector((state) => state.cafe.text);
+  console.log('데이터:', cafeinfo);
 
   if (cafeinfo !== undefined) {
     axios({
-      method: "get",
+      method: 'get',
       url: 'http://localhost:3001/imageSearch',
       params: {
-        value: cafeinfo[0].place_name
-      }
+        value: cafeinfo[0].place_name,
+      },
     }).then((searchdata) => {
-      if (searchdata.status !== 200) return alert('통신에러')
+      if (searchdata.status !== 200) return alert('통신에러');
       console.log(searchdata.data.items[0].link);
-    })
+    });
   }
-
-
 
   return (
     <>
-      {cafeinfo == undefined ? <span></span> :
+      {cafeinfo == undefined ? (
+        <span></span>
+      ) : (
         cafeinfo.map((info, index) => {
           return (
-            <div className='contents-box' key={index}>
-              <a className='hvr-grow' href={info.place_url}>
+            <div className="contents-box" key={index}>
+              <a className="hvr-grow" href={info.place_url}>
                 {info.place_name}
               </a>
               <br />
               <br />
-              <div style={{ fontSize: '14px' }}>
-                {info.phone}
-              </div>
+              <div style={{ fontSize: '14px' }}>{info.phone}</div>
             </div>
-          )
+          );
         })
-      }
+      )}
     </>
-  )
+  );
 }
--- cafeinfoyou/src/components/FilterBox.jsx
+++ cafeinfoyou/src/components/FilterBox.jsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { cyan, green, indigo, pink, red } from '@mui/material/colors';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { height } from '@mui/system';
-
 
 export default function FilterBox() {
   const dispatch = useDispatch();
@@ -17,105 +12,97 @@ export default function FilterBox() {
   });
 
   return (
-    <FormControl sx={{ m: 2, }} component="fieldset" variant="standard">
-      {/* <FormLabel component="legend">Assign responsibility</FormLabel> */}
-      <FormGroup sx={{ flexDirection: 'row' }} >
-        <FormControlLabel
-          control={
-            <Checkbox
-              size="small"
-              checked={state.review}
-              onChange={() => {
-                dispatch({
-                  type: 'filter/CHANGE',
-                  payload: { name: 'review', checked: !state.review },
-                });
-              }}
-              name="리뷰순"
-
-              sx={{
-                color: indigo['A700'],
-                '&.Mui-checked': {
-                  color: indigo[600],
-                },
-                width: '30px',
-              }}
-            />
-          }
-          label="리뷰순"
-          sx={{
-            border: 3,
-            borderColor: indigo['A700'],
-            borderRadius: 2,
-            bgcolor: indigo[50],
-            pr: 0.5,
-            pt: 0,
-            width: '75px'
-          }}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              size="small"
-              checked={state.large}
-              onChange={() => {
-                dispatch({
-                  type: 'filter/CHANGE',
-                  payload: { name: 'large', checked: !state.large },
-                });
-              }}
-              name="대형"
-              sx={{
-                color: green['A200'],
-                '&.Mui-checked': {
-                  color: green[600],
-                },
-                width: '30px',
-
-              }}
-            />
-          }
-          label="대형"
-          sx={{
-            border: 3,
-            borderColor: green['A200'],
-            borderRadius: 2,
-            bgcolor: cyan[50],
-            pr: 1,
-          }}
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              size="small"
-              checked={state.dessert}
-              onChange={() => {
-                dispatch({
-                  type: 'filter/CHANGE',
-                  payload: { name: 'dessert', checked: !state.dessert },
-                });
-              }}
-              name="디저트"
-              sx={{
-                color: pink['A200'],
-                '&.Mui-checked': {
-                  color: pink[600],
-                },
-                width: '30px',
-
-              }}
-            />
-          }
-          label="디저트"
-          sx={{
-            border: 3,
-            borderColor: pink['A200'],
-            borderRadius: 2,
-            bgcolor: red[50],
-            pr: 1,
-          }}
-        />
-      </FormGroup>
-    </FormControl>
+    <>
+      <FormControlLabel
+        control={
+          <Checkbox
+            size="small"
+            checked={state.review}
+            onChange={() => {
+              dispatch({
+                type: 'filter/CHANGE',
+                payload: { name: 'review', checked: !state.review },
+              });
+            }}
+            name="리뷰순"
+            sx={{
+              color: indigo['A700'],
+              '&.Mui-checked': {
+                color: indigo[600],
+              },
+              width: '30px',
+            }}
+          />
+        }
+        label="리뷰순"
+        sx={{
+          borderRadius: 2,
+          bgcolor: indigo[50],
+          ml: 2.7,
+          mt: 3,
+          pr: 0.5,
+          pt: 0,
+          width: '80px',
+        }}
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            size="small"
+            checked={state.large}
+            onChange={() => {
+              dispatch({
+                type: 'filter/CHANGE',
+                payload: { name: 'large', checked: !state.large },
+              });
+            }}
+            name="대형"
+            sx={{
+              color: green['A200'],
+              '&.Mui-checked': {
+                color: green[600],
+              },
+              width: '30px',
+            }}
+          />
+        }
+        label="대형"
+        sx={{
+          borderRadius: 2,
+          bgcolor: cyan[50],
+          mt: 3,
+          pr: 1,
+        }}
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            size="small"
+            checked={state.dessert}
+            onChange={() => {
+              dispatch({
+                type: 'filter/CHANGE',
+                payload: { name: 'dessert', checked: !state.dessert },
+              });
+            }}
+            name="디저트"
+            sx={{
+              color: pink['A200'],
+              '&.Mui-checked': {
+                color: pink[600],
+              },
+              width: '30px',
+            }}
+          />
+        }
+        label="디저트"
+        sx={{
+          borderRadius: 2,
+          bgcolor: red[50],
+          mt: 3,
+          pr: 1,
+        }}
+      />
+    </>
   );
 }
