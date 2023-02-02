4f0bc37fa83a3f2dda09c28cf1b08d9be385664f
박현목 <phmok1@gmail.com>
2023-02-02 17:36:15 +0900

    image

--- cafeinfoyou/src/components/Data.jsx
+++ cafeinfoyou/src/components/Data.jsx
@@ -4,6 +4,8 @@ import cafe from '../store/modules/cafe';
 import '../styles/sidebar.css';
 import axios from 'axios'
 import { useEffect, useState } from 'react';
+import Iframe from 'react-iframe'
+// import { isMobile } from 'react-device-detect';
 
 export default function Data() {
 
@@ -11,71 +13,52 @@ export default function Data() {
   const [img, setImg] = useState([]);
 
   useEffect(() => {
-    initialize()
-  }, [])
-
+    initialize();
+  }, cafeinfo)
 
   function initialize() {
     if (cafeinfo == undefined) return;
     let imgArr = []
 
     cafeinfo.map((info, i) => {
-      axios({
-        method: "get",
-        url: 'http://localhost:3001/imageSearch',
-        params: {
-          value: cafeinfo[i].place_name
-        }
-      }).then((searchdata) => {
-        if (searchdata.status !== 200) return alert('통신에러')
-        // console.log(searchdata.data.items[0].link);
-        imgArr.push(searchdata.data.items[0].link);
-        console.log(searchdata)
-      })
-
+      setTimeout(function () {
+        axios({
+          method: "get",
+          url: 'http://localhost:3001/imageSearch',
+          params: {
+            value: cafeinfo[i].place_name
+          }
+        }).then((searchdata) => {
+          if (searchdata.status !== 200) return alert('통신에러')
+          // console.log(searchdata.data.items[0].link);
+          imgArr.push(searchdata.data.items[0].link);
+          setImg([...imgArr])
+        })
+      }, 180 * (i + 1));
     })
 
-    setImg(imgArr)
-
   }
-  console.log('데이터:', cafeinfo)
-
-
-  // if (cafeinfo !== undefined) {
-  //   axios({
-  //     method: "get",
-  //     url: 'http://localhost:3001/imageSearch',
-  //     params: {
-  //       value: cafeinfo[0].place_name
-  //     }
-  //   }).then((searchdata) => {
-  //     if (searchdata.status !== 200) return alert('통신에러')
-  //     console.log(searchdata.data.items[0].link);
-  //   })
-  // }
-
-
-
 
   return (
     <>
       {img.length}
       {cafeinfo == undefined ? <span></span> :
         cafeinfo.map((info, index) => {
+          let url = info.place_url;
+          // if (isMobile) url = info.place_url.slice(0, 26) + '/m' + info.place_url.slice(26);
+
           return (
             <div className='contents-box' key={index}>
-
-              <a className='hvr-grow' href={info.place_url} target='_blank'>
-
+              <a className='hvr-grow' href={url} target='_blank'>
                 {info.place_name}
               </a>
               <br />
-              <br />
-              <div style={{ fontSize: '14px' }}>
+              <div style={{ fontSize: '12px' }}>
                 {info.phone}
               </div>
+              <br />
               {img.length > 0 && (
-                <img style={{ width: '200px' }} src={img[index]}></img>
+                <img style={{ borderRadius: '4px', width: '230px', height: '120px' }} src={img[index]}></img>
               )}
             </div>
           )
