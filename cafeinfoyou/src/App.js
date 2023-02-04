7179292b4c8778896edb548075bfc986ccc732e5
HaeRi Seo <jackie100425@naver.com>
2023-02-04 12:17:47 +0900

    feat: 코드 정리

--- cafeinfoyou/src/App.js
+++ cafeinfoyou/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Blog from './apis/Blog';
-import FilterBox from './components/FilterBox';
 import KakaoMap from './components/KakaoMap';
 import Sidebar from './components/Sidebar';
 
